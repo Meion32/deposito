d8758e01b55d2596cc2452da53868572dae67bf3
Meion <konkerik32@gmail.com>
2026-07-22 23:52:34 -0300

    MP3 melhorado

--- MP3_artesanal.py
+++ MP3_artesanal.py
@@ -8,6 +8,14 @@ import pygame
 pygame.init()
 pygame.mixer.init()
 
+def hub():
+    print ("-" * 50)
+    print ("1) Pausar")
+    print ("2) Continuar")
+    print ("3) Parar")
+    print ("4) Escolher música")
+    print ("5) Sair")
+    print ("-" * 50)
 #busca pela pasta de musicas e criação de playlist
 pasta_musicas = "musicas"
 playlist = []
@@ -16,24 +24,57 @@ for arquivo in os.listdir(pasta_musicas):
         playlist.append(
             os.path.join (pasta_musicas,arquivo) 
         )
-print (playlist)
-
 #Escolher qual musica tocar 
-while True:
-    try:
-        escolha = int(input("Qual musica você quer tocar?"))
-        break
-    except ValueError:
-        print ("Coloque apenas númer inteiros como resposta")
-
-#Validando escolha e rodando a musica
-if (0 < escolha) and (escolha <= len(playlist)):
-    atual = playlist[escolha - 1]
-    print("Carregando:", atual)
-    pygame.mixer.music.load(atual)
+def escolher(playlist):        
+    total = len(playlist)
+    n = int(0)
+    while True:
+        while True:
+            try:
+                nome = os.path.basename(playlist[n])
+                break
+            except IndexError:
+                n -= n
+        print (f"{n + 1}/{total}")
+        print (nome)
+        seleção = input("< o >\n").strip().lower()
+        match seleção:
+            case "o":
+                return n
+            case ">":
+                n += 1
+            case "<":
+                if n == 0:
+                    print ("Você ja esta na primeira musica")    
+                else:
+                    n-= 1
+def tocar(indice):
+    toca = playlist[indice]
+    pygame.mixer.music.stop()
+    print("Carregando:", os.path.basename(toca))
+    pygame.mixer.music.load(toca)
     print("Música carregada!")
     pygame.mixer.music.play()
     print("Comando play enviado!")
-    input ("preciso ENTER para acabar a musica")
-else:
-    print (f"Coloque um número entre 1 e {len(playlist)}") 
+while True:
+    hub()
+    while True:
+        try:
+            player = int(input("\n"))
+            break
+        except ValueError:
+            print ("Escolha apenas um número válido")
+    match player:
+        case 1:
+            pygame.mixer.music.pause()
+        case 2:
+            pygame.mixer.music.unpause()
+        case 3:
+            pygame.mixer.music.stop()
+        case 4:
+            indice = escolher(playlist)
+            tocar(indice)
+        case 5:
+            print ("Volte quando necessario")
+            time.sleep(1)
+            break
